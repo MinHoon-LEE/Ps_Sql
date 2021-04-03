57cf319d1d21809c11e9099f386a3db70b87e7b9
MinHoon-LEE <lucky3749@naver.com>
2021-04-03 15:47:00 +0900

    2021-04-03

---
+++ Programmers/Algorithm/Python/42748/42748.py
@@ -0,0 +1,8 @@
+def solution(array, commands):
+    answer = []
+    for i in range (len(commands)):
+        tmp = []
+        tmp = array[commands[i][0] -1  : commands[i][1]]
+        tmp.sort()
+        answer.append(tmp[commands[i][2] - 1])
+    return answer
