f34b0636e62419044ab08c21a7fe04da93efd0c8
minlee <lucky3749@naver.com>
2021-12-06 17:40:47 +0900

    2021-12-06

---
+++ Programmers/Algorithm/Python/84512/84512.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+def count_word(num):
+    value = 0 
+    for i in range(num):
+        value = 5 * value + 1
+    return value
+    
+def solution(word):
+    answer = 0
+    print(word)
+    arr = defaultdict(str)
+    arr['A'] = 0
+    arr['E'] = 1
+    arr['I'] = 2
+    arr['O'] = 3
+    arr['U'] = 4
+    length = len(word)
+    for i in range(length):
+        answer += arr[word[i]] * count_word(5 - i) + 1    
+    return answer
